8766fc26794fecdbd31a2f9125f60d468a901ef1
Thener <thenerthener@gmail.com>
2019-04-12 15:38:12 +0200

    initial commit

---
+++ app/src/main/java/com/thener/animation/MainActivity.java
@@ -0,0 +1,20 @@
+package com.thener.animation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startActivity(new Intent(MainActivity.this, Welcome.class));
+    }
+}
